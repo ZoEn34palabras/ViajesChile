1208ca97ac7109d6b67c8a4ab608822210e24aa0
zoen34palabras <enzo.umaguma@gmail.com>
2024-04-10 19:21:35 -0400

    commit inicial

---
+++ assets/js/script.js
@@ -0,0 +1,25 @@
+/* const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+
+$( "#enviarCorreo" ).on( "click", function() {
+    alert("El correo fue enviado correctamente...");
+} );
+
+$(document).ready(function() {
+    $(".ingre").dblclick(function() {
+    $(this).css("color", "red");
+    });
+});
+
+$(document).ready(function() {
+    $(".prep").dblclick(function() {
+    $(this).css("color", "red");
+    });
+});
+
+
+$( ".card" ).on( "click", function() {
+    $(this).toggle( "slow", function() {
+    });
+});
+*/ 
