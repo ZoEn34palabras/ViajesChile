f173abd18ec0cd156546e862f6f80f22f5f73531
zoen34palabras <enzo.umaguma@gmail.com>
2024-04-15 12:18:06 -0400

    Agrega funcionalidades javascript

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,25 +1,6 @@
-/* const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
 const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
 
 $( "#enviarCorreo" ).on( "click", function() {
-    alert("El correo fue enviado correctamente...");
+    alert("Gracias por contactarnos");
 } );
-
-$(document).ready(function() {
-    $(".ingre").dblclick(function() {
-    $(this).css("color", "red");
-    });
-});
-
-$(document).ready(function() {
-    $(".prep").dblclick(function() {
-    $(this).css("color", "red");
-    });
-});
-
-
-$( ".card" ).on( "click", function() {
-    $(this).toggle( "slow", function() {
-    });
-});
-*/ 
